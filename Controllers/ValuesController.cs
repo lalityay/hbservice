0a934e181977d0a7300476807809dae0af452088
lalityay <lalityay@gmail.com>
2019-10-24 20:45:18 +0530

    made some changes

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -31,11 +31,22 @@ namespace hbservice.Controllers
             //customPages.Add("company/branchCompetitors?ID=100369");
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
-            RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://172.17.0.2:4445/wd/hub/"), options.ToCapabilities(), TimeSpan.FromSeconds(600));
-            driver.Navigate().GoToUrl("chrome-extension://cilllgpaahohggfdioeinnncmaecaeni/icon.png");
-            var UserCookie = System.IO.File.ReadAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "cookie.txt"));
+            RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://10.0.2.15:31799/wd/hub/"), options.ToCapabilities(), TimeSpan.FromSeconds(600));
+            //driver.Navigate().GoToUrl("chrome-extension://cilllgpaahohggfdioeinnncmaecaeni/icon.png");
+            driver.Navigate().GoToUrl("https://www.snlnet.com/snl.services.export.service/whoami.asp");
 
-            (driver).ExecuteScript("localStorage.setItem('profiles', JSON.stringify({Cookie:'" + UserCookie + "'}));");
+            var UserCookie = System.IO.File.ReadAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "cookie.txt"));
+           // CookieContainer ccon = new CookieContainer();
+           // ccon.SetCookies(new Uri("https://www.snlnet.com"), UserCookie);
+
+            foreach (var item in CookieValue(UserCookie))
+            {
+                OpenQA.Selenium.Cookie ck = new OpenQA.Selenium.Cookie(item.Key, item.Value);
+
+                driver.Manage().Cookies.AddCookie(ck);
+            }
+
+            //(driver).ExecuteScript("localStorage.setItem('profiles', JSON.stringify({Cookie:'" + UserCookie + "'}));");
             List<string> manifestList = new List<string>();
             for (int i = 0; i < customPages.Count; i++)
             {
@@ -61,7 +72,16 @@ namespace hbservice.Controllers
             manifestList.Add(", Total Elapsed seconds:" + watch.Elapsed.TotalSeconds);
             return manifestList;
         }
+        
+private Dictionary<string,string> CookieValue(string header)
+        {
+            var parts = header.Split(';')
+                .Where(i => i.Contains("="))
+                .Select(i => i.Trim().Split('='))
+                .ToDictionary(i => i.First(), i => i.Last());
 
+            return parts;
+        }
 
     }
 }
